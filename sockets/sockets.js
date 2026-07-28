286cd0ad4927ea1047d6ea2aeec3cdefa0ccd8c9
Kevin Lacán <krlacan@cosami.net>
2026-07-28 11:51:54 -0600

    agregar, eliminar y votar

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,14 +1,39 @@
 const { io } = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
 
 //Messages sockets
 io.on('connection', client => {
     console.log('Client connected');
+
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => { console.log('Client disconnected'); });
+
     client.on('mensaje', (payload) => {
         console.log('Mensaje', payload);
         io.emit('mensaje', { admin: 'New message' });
     });
+
     client.on('emitir-mensaje', (payload) => {
         client.broadcast.emit('emitir-mensaje', payload);
+    });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.removeBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     });
 });
